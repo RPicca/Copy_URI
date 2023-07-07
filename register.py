893beb1a36a869d4bb1e29e6ad9b80a427b0bfb2
Romain PICCA <rpicca@forsk.com>
2023-07-07 14:30:49 +0200

    Oubli de gestion des espaces...

--- register.py
+++ register.py
@@ -1,5 +1,6 @@
 import winreg as reg
 import os
+import sys
 #Creation clé pour fichier
 if getattr(sys, 'frozen', False):
     naos_share = os.path.dirname(os.path.dirname(sys.executable))
@@ -8,11 +9,11 @@ elif __file__:
 key=reg.CreateKey(reg.HKEY_CURRENT_USER, "SOFTWARE\\Classes\\*\\shell\\URI_NAOS")
 reg.SetValue(key, '', reg.REG_SZ, "URI_NAOS")
 reg.SetValueEx(key, 'Icon', 0, reg.REG_SZ, naos_share+"\\URI_NAOS\\naos.ico")
-reg.SetValue(key, 'command', reg.REG_SZ, "\""+naos_share+"\\URI_NAOS\\copy_naos.exe\" %1")
+reg.SetValue(key, 'command', reg.REG_SZ, "\""+naos_share+"\\URI_NAOS\\copy_naos.exe\" \"%1\"")
 
 #Creation clé pour dossier
 key=reg.CreateKey(reg.HKEY_CURRENT_USER, "SOFTWARE\\Classes\\directory\\shell\\URI_NAOS")
 reg.SetValue(key, '', reg.REG_SZ, "URI_NAOS")
 reg.SetValueEx(key, 'Icon', 0, reg.REG_SZ, naos_share+"\\URI_NAOS\\naos.ico")
-reg.SetValue(key, 'command', reg.REG_SZ, "\""+naos_share+"\\URI_NAOS\\copy_naos.exe\" %1")
+reg.SetValue(key, 'command', reg.REG_SZ, "\""+naos_share+"\\URI_NAOS\\copy_naos.exe\" \"%1\"")
 
