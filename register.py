71b17d0a0acc2f4b9bb8c808ee4a3b6f24e2823a
Romain PICCA <rpicca@forsk.com>
2024-10-01 13:49:07 +0200

    plus d'utilisation de pyperclip et passage en edition de registre pour utilisation direct de fichiers .py

--- register.py
+++ register.py
@@ -1,19 +1,18 @@
 import winreg as reg
 import os
 import sys
-#Creation clé pour fichier
+# Creation clé pour fichier
 if getattr(sys, 'frozen', False):
     naos_share = os.path.dirname(os.path.dirname(sys.executable))
 elif __file__:
     naos_share = os.path.dirname(os.path.dirname(__file__))
-key=reg.CreateKey(reg.HKEY_CURRENT_USER, "SOFTWARE\\Classes\\*\\shell\\URI_NAOS")
+key = reg.CreateKey(reg.HKEY_CURRENT_USER, "SOFTWARE\\Classes\\*\\shell\\URI_NAOS")
 reg.SetValue(key, '', reg.REG_SZ, "URI_NAOS")
-reg.SetValueEx(key, 'Icon', 0, reg.REG_SZ, naos_share+"\\URI_NAOS\\naos.ico")
-reg.SetValue(key, 'command', reg.REG_SZ, "\""+naos_share+"\\URI_NAOS\\copy_naos.exe\" \"%1\"")
+reg.SetValueEx(key, 'Icon', 0, reg.REG_SZ, naos_share + "\\URI_NAOS\\naos.ico")
+reg.SetValue(key, 'command', reg.REG_SZ, "py \"" + naos_share + "\\URI_NAOS\\copy_naos.py\" \"%1\"")
 
-#Creation clé pour dossier
-key=reg.CreateKey(reg.HKEY_CURRENT_USER, "SOFTWARE\\Classes\\directory\\shell\\URI_NAOS")
+# Creation clé pour dossier
+key = reg.CreateKey(reg.HKEY_CURRENT_USER, "SOFTWARE\\Classes\\directory\\shell\\URI_NAOS")
 reg.SetValue(key, '', reg.REG_SZ, "URI_NAOS")
-reg.SetValueEx(key, 'Icon', 0, reg.REG_SZ, naos_share+"\\URI_NAOS\\naos.ico")
-reg.SetValue(key, 'command', reg.REG_SZ, "\""+naos_share+"\\URI_NAOS\\copy_naos.exe\" \"%1\"")
-
+reg.SetValueEx(key, 'Icon', 0, reg.REG_SZ, naos_share + "\\URI_NAOS\\naos.ico")
+reg.SetValue(key, 'command', reg.REG_SZ, "py \"" + naos_share + "\\URI_NAOS\\copy_naos.py\" \"%1\"")
